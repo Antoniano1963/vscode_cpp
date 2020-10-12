f3223f3d3fe6a96e9c47f1cef96d284b7601afaf
吕昊泽 <2221927869@qq.com>
2020-10-12 21:53:48 +0800

    0

--- DSAA/Lab3/assignment4.cpp
+++ DSAA/Lab3/assignment4.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namespace std;
+
+void mergeSort(int begin, int end, int *array0, int *array1);
+void countFunc(int begin, int end, int middle, int *array0, int *array1);
 int main(){
     int n;
     cin >> n;
@@ -8,10 +11,13 @@ int main(){
     for (int i = 0; i < n;i++){
         cin >> array0[n];
     }
-
+    mergeSort(0, n - 1, array0, array1);
+    for (int i = 0; i < n;i++){
+        cout << array0[i] + " ";
+    }
 }
 void mergeSort(int begin,int end,int *array0,int *array1){
-    if(begin>end){
+    if(begin>=end){
         return;
     }
     int middle = (end - begin) / 2 + begin;
@@ -25,5 +31,28 @@ void countFunc(int begin,int end,int middle,int *array0,int *array1){
     int num1 = begin;
     int num2 = middle + 1;
     int k = begin; 
-
+    while((num1<=middle)&&(num2<=end)){
+        if(array0[num1]>array0[num2]){
+            array1[k] = array0[num2];
+            k++;
+            num2++;
+        }else{
+            array1[k] = array0[num1];
+            k++;
+            num1++;
+        }
+    }
+    while (num1<=middle)
+    {
+        array1[k] = array0[num1];
+        num1++;
+    }
+    while(num2<=end){
+        array1[k] = array0[num2];
+        k++;
+        num2++;
+    }
+    for (int i = begin; i <= end;i++){
+        array0[i] = array1[i];
+    }
 }
